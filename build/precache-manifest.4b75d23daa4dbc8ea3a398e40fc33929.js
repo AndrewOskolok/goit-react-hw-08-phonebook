eaa14ec133ad3f2fb64f3da99366fbdc5caf9961
Oscolok <oscolok7@gmail.com>
2020-08-13 16:09:21 +0300

    hw is done

---
+++ build/precache-manifest.4b75d23daa4dbc8ea3a398e40fc33929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab155576f18f42ac09570b52e94ed635",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "608902e56abf031afac5",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.b36146cf.chunk.css"
+  },
+  {
+    "revision": "916b6b629e2e22ed3fc9",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.25d8d264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.25d8d264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608902e56abf031afac5",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.c6480f80.chunk.js"
+  },
+  {
+    "revision": "a92f4659468ade53de79",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.f63d84e7.js"
+  }
+]);
